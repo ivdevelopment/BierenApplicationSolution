47a8f08e3daaadbb64b52ee88f066385f13afcf9
IanV <ianverlinden1.iv@gmail.com>
2022-10-27 13:57:14 +0200

    Add project files.

---
+++ BierenApplication/Controllers/BierController.cs
@@ -0,0 +1,14 @@
+﻿using BierenApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BierenApplication.Controllers
+{
+    public class BierController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Bier> bieren = new() { new Bier { Id = 1, Naam = "Romy Pils", Alcohol = 5f }, new Bier { Id = 2, Naam = "Kasteelbier", Alcohol = 12f }, new Bier { Id = 3, Naam = "Maes Radler", Alcohol = 0f } };
+            return View(bieren);
+        }
+    }
+}
---
+++ BierenApplication/Models/Bier.cs
@@ -0,0 +1,9 @@
+﻿namespace BierenApplication.Models
+{
+    public class Bier
+    {
+        public int Id { get; set; }
+        public string Naam { get; set; }
+        public float Alcohol { get; set; }
+    }
+}
